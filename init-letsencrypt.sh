c7afa520418464b12a08514796f608e4763f7f24
Oğuzhan Subaşı <subasioguz@gmail.com>
2022-07-12 18:17:06 +0300

    domain eklendi

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=storage.hizmetmerkezim.com
+domains=(storage.hizmetmerkezim.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="subasioguz@gmail.com" # Adding a valid address is strongly recommended
